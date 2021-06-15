fef147a0195e6dc932af3d02130fb9857c8c421e
Saba Zikaria <szikaria961@gmail.com>
2021-06-15 14:51:12 -0500

    Generate QR code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import { Container } from '@material-ui/core';
+import { Button, Container, TextField } from '@material-ui/core';
 import QrReader from 'react-qr-reader';
+import QrCode from 'qrcode';
 
 function App() {
   const [scannedItem, setScannedItem] = useState('');
+  const [url, setUrl] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
 
   const handleScan = (data) => {
     if (data) {
@@ -15,8 +18,22 @@ function App() {
   const handleError = (error) => {
     console.log(error);
   }
+
+  const generateQrCode = async () => {
+    try {
+      const response = await QrCode.toDataURL(url);
+      setImageUrl(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <Container>
+      <h3>Generate QR code</h3>
+      <TextField label='Text' onChange={(e)=> setUrl(e.target.value)}/>
+      <Button variant='contained' color='primary' onClick={() => generateQrCode()}> Generate</Button>
+      <br/> <br/> <br/>
+      {imageUrl ? (<img src={imageUrl} alt="img" />) : null}
       <h3>Scan a QR code using your Webcam</h3>
       <QrReader 
         delay={300}
